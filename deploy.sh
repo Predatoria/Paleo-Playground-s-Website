df39c046b5a32a153201ce5ccec7c0cf20476aa7
Kenny Jones <Veronica@10.2.70.93>
2017-03-15 17:51:54 -0400

    Rearranging deployment and file locations

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clean the slate for git
+git checkout master
+git reset --hard origin/master
+
+# Then, pull new content
+cd /var/www/paleo
+git pull
+
+# Now, rebuild the files for main site
+cd /var/www/paleo/main/
+rm -r public
+hugo
+
+# Now, rebuild the files for blog site
+cd /var/www/paleo/blog/
+rm -r public
+hugo
--- github-pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /var/www/paleo
-git pull
